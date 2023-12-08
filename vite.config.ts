ceba8921bc26ee542fd5cf62af1691d72459c46c
a4ghazal <a4ghazal@uwaterloo.ca>
2023-12-08 05:31:57 -0500

    depolyment attempt

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/Portfolio-V2",
+    base: "./Portfolio-V2/",
     // base: "./",
     plugins: [react(), svgr()],
 });
